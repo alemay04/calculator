a3e05c15a47d6aa9b00be2c19bfc162d7e0abc81
Alex Lemay <alemay8@hotmail.ca>
2024-01-16 14:07:24 -0500

    Finish the logic of the calculator

--- script.js
+++ script.js
@@ -1,21 +1,82 @@
+const display = document.querySelector("#display");
+const keys = document.querySelectorAll("button");
 
-function add(a, b){
-    return (a + b);
-}
-function substract(a,b){
-    return (a - b);
-}
-function multiply(a, b){
-    return (a * b);
-}
-function divid(a, b){
-    return(a / b);
-}
+ let currentValue = "";
+ let operator = null;
+ let previousValue = null;
 
-let firstNum = "0";
-let operator = "+";
-let secondNum = "0";
+ function operate(button){
+    const value = button.textContent;
+    if (value === "AC"){
+        currentValue = "";
+        operator = null;
+        previousValue = null;
+    }
+    else if (value === "="){
+        if(operator && previousValue !== null){
+            currentValue = calculateResult(previousValue,operator,currentValue);
+            operator = null;
+            previousValue = null;
 
-function operate(){
-    return operator(firstNum, secondNum);
-}
+        }
+       
+    }
+    else if (isOperator(value)){
+        if (operator && previousValue !== null){
+            operator = value;
+            previousValue = currentValue;
+        }
+        else {
+            operator = value;
+            previousValue = currentValue;
+            currentValue = "";
+        }
+    }
+    else if (value === "%"){
+        currentValue = calculatePercentage(currentValue);
+    }
+    else if (value === "+/-"){
+        currentValue = changeSign(currentValue);
+    }
+    else{
+      currentValue = currentValue === 0 ? value : currentValue + value ;
+    }
+
+    if (currentValue.length > 9) {
+        currentValue = currentValue.slice(0, 9);
+    }
+    display.textContent = currentValue;
+
+ }
+function isOperator(value) {
+    return ['+', '-', '*', '/'].includes(value);
+ }
+function calculateResult(num1, operator, num2) {
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+
+    switch (operator) {
+      case '+':
+        return num1 + num2;
+      case '-':
+        return num1 - num2;
+      case '*':
+        return num1 * num2;
+      case '/':
+        return num2 !== 0 ? num1 / num2 : "Error";
+      default:
+        return "Error";
+    }
+ }
+ function calculatePercentage(value) {
+    return parseFloat(value) / 100;
+ }
+ function changeSign(value) {
+    return changeSign(value) * -1;
+ }
+
+keys.forEach(button => {
+    button.addEventListener("click", () => {
+        operate(button);
+    })
+})
